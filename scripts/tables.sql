be80573856e65e1e70ec36355f078e8b1c26b78c
Willsson10 <104332577+Willsson10@users.noreply.github.com>
2024-09-01 23:10:47 -0300

    Update tables.sql

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -16,61 +16,67 @@ CREATE TABLE vinhos.vinhos (
     preco DECIMAL(10, 2) NOT NULL,
     quantidade_estoque INT DEFAULT 0,
     img_path VARCHAR(255),
-    CONSTRAINT pk_nome PRIMARY KEY (nome)
+    CONSTRAINT pk_vinho PRIMARY KEY (nome, vinicula) -- Chave primária composta por nome e vinícola
 );
 
 CREATE TABLE vinhos.vinho_tinto (
     nome_vinho VARCHAR(255),
+    vinicula VARCHAR(50),
     estilo VARCHAR(50) NOT NULL,
     CONSTRAINT ck_estilo CHECK (estilo IN ('Cabernet Sauvignon', 'Merlot', 'Pinot Noir', 'Syrah')),
-    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho)
-        REFERENCES vinhos.vinhos(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT pk_vinho_tinto PRIMARY KEY (nome_vinho)
+    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho, vinicula)
+        REFERENCES vinhos.vinhos(nome, vinicula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT pk_vinho_tinto PRIMARY KEY (nome_vinho, vinicula)
 );
 
 CREATE TABLE vinhos.vinho_branco (
     nome_vinho VARCHAR(255),
+    vinicula VARCHAR(50),
     estilo VARCHAR(50) NOT NULL,
     CONSTRAINT ck_estilo CHECK (estilo IN ('Chardonnay', 'Sauvignon Blanc', 'Riesling', 'Pinot Grigio')),
-    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho)
-        REFERENCES vinhos.vinhos(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT pk_vinho_branco PRIMARY KEY (nome_vinho)
+    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho, vinicula)
+        REFERENCES vinhos.vinhos(nome, vinicula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT pk_vinho_branco PRIMARY KEY (nome_vinho, vinicula)
 );
 
 CREATE TABLE vinhos.vinho_espumante (
     nome_vinho VARCHAR(255),
+    vinicula VARCHAR(50),
     estilo VARCHAR(50) NOT NULL,
     CONSTRAINT ck_estilo CHECK (estilo IN ('Champagne', 'Prosecco', 'Cava', 'Espumante Rosé')),
-    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho)
-        REFERENCES vinhos.vinhos(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT pk_vinho_espumante PRIMARY KEY (nome_vinho)
+    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho, vinicula)
+        REFERENCES vinhos.vinhos(nome, vinicula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT pk_vinho_espumante PRIMARY KEY (nome_vinho, vinicula)
 );
 
 CREATE TABLE vinhos.vinho_rose (
     nome_vinho VARCHAR(255),
+    vinicula VARCHAR(50),
     estilo VARCHAR(50) NOT NULL,
     CONSTRAINT ck_estilo CHECK (estilo IN ('Provence Rosé', 'Tempranillo Rosé', 'Zinfandel Rosé', 'Grenache Rosé')),
-    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho)
-        REFERENCES vinhos.vinhos(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT pk_vinho_rose PRIMARY KEY (nome_vinho)
+    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho, vinicula)
+        REFERENCES vinhos.vinhos(nome, vinicula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT pk_vinho_rose PRIMARY KEY (nome_vinho, vinicula)
 );
 
 CREATE TABLE vinhos.vinho_sobremesa (
     nome_vinho VARCHAR(255),
+    vinicula VARCHAR(50),
     estilo VARCHAR(50) NOT NULL,
     CONSTRAINT ck_estilo CHECK (estilo IN ('Sauternes', 'Porto Branco', 'Moscato d''Asti', 'Tokaji')),
-    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho)
-        REFERENCES vinhos.vinhos(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT pk_vinho_sobremesa PRIMARY KEY (nome_vinho)
+    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho, vinicula)
+        REFERENCES vinhos.vinhos(nome, vinicula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT pk_vinho_sobremesa PRIMARY KEY (nome_vinho, vinicula)
 );
 
 CREATE TABLE vinhos.vinho_fortificado (
     nome_vinho VARCHAR(255),
+    vinicula VARCHAR(50),
     estilo VARCHAR(50) NOT NULL,
     CONSTRAINT ck_estilo CHECK (estilo IN ('Porto (Ruby)', 'Porto (Tawny)', 'Xerez (Sherry)', 'Madeira')),
-    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho)
-        REFERENCES vinhos.vinhos(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT pk_vinho_fortificado PRIMARY KEY (nome_vinho)
+    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho, vinicula)
+        REFERENCES vinhos.vinhos(nome, vinicula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT pk_vinho_fortificado PRIMARY KEY (nome_vinho, vinicula)
 );
 
 CREATE TABLE vinhos.carrinho (
@@ -85,13 +91,14 @@ CREATE TABLE vinhos.carrinho (
 CREATE TABLE vinhos.carrinho_produto (
     numero_carrinho INT,
     nome_vinho VARCHAR(255),
+    vinicula VARCHAR(50),
     quantidade INT DEFAULT 0,
     subtotal DECIMAL(10, 2) DEFAULT 0,
     CONSTRAINT fk_numero_carrinho FOREIGN KEY (numero_carrinho)
         REFERENCES vinhos.carrinho(numero) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho)
-        REFERENCES vinhos.vinhos(nome) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT pk_carrinho_produto PRIMARY KEY (numero_carrinho, nome_vinho)
+    CONSTRAINT fk_nome_vinho FOREIGN KEY (nome_vinho, vinicula)
+        REFERENCES vinhos.vinhos(nome, vinicula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT pk_carrinho_produto PRIMARY KEY (numero_carrinho, nome_vinho, vinicula)
 );
 
 CREATE TABLE vinhos.compras (
@@ -104,11 +111,12 @@ CREATE TABLE vinhos.compra_carrinho_produto (
     numero_compra INT,
     numero_carrinho INT,
     nome_vinho VARCHAR(255),
+    vinicula VARCHAR(50),
     quantidade INT DEFAULT 0,
     subtotal DECIMAL(10, 2) DEFAULT 0,
     CONSTRAINT fk_numero_compra FOREIGN KEY (numero_compra)
         REFERENCES vinhos.compras(numero) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT fk_carrinho_vinho FOREIGN KEY (numero_carrinho, nome_vinho)
-        REFERENCES vinhos.carrinho_produto(numero_carrinho, nome_vinho) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT pk_compra_carrinho_produto PRIMARY KEY (numero_compra, numero_carrinho, nome_vinho)
+    CONSTRAINT fk_carrinho_vinho FOREIGN KEY (numero_carrinho, nome_vinho, vinicula)
+        REFERENCES vinhos.carrinho_produto(numero_carrinho, nome_vinho, vinicula) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT pk_compra_carrinho_produto PRIMARY KEY (numero_compra, numero_carrinho, nome_vinho, vinicula)
 );
