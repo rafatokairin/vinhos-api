a23b660eb39fd0eba3f3d1e3135e7acc31f06fad
Willsson10 <franciscobgmugnaini@gmail.com>
2024-09-19 15:47:46 -0300

    apagando muita coisa

--- vinhos/src/main/java/com/vinhos/bd/controller/MyAppUserController.java
+++ vinhos/src/main/java/com/vinhos/bd/controller/MyAppUserController.java
@@ -22,7 +22,7 @@ import java.sql.SQLException;
 public class MyAppUserController extends HttpServlet {
 
     @Override
-    protected void doPost (HttpServletRequest request, HttpServletResponse response)
+     protected void doPost (HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         DAO<MyAppUser, String> dao;
