2cdca8d3684f108cf2910add3f29a1993e66a72b
rafatokairin <rptokairin@gmail.com>
2024-09-21 14:06:47 -0300

    finishing PgVinhoDAO

--- bd/src/main/java/com/vinhos/bd/dao/PgVinhoDAO.java
+++ bd/src/main/java/com/vinhos/bd/dao/PgVinhoDAO.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class PgVinhoDAO implements VinhoDAO{
+public class PgVinhoDAO implements VinhoDAO {
 
     private final Connection connection;
 
@@ -25,6 +25,7 @@ public class PgVinhoDAO implements VinhoDAO{
                     "AND (categoria = ? OR ? IS NULL) " +
                     "AND (estilo = ? OR ? IS NULL) " +
                     "AND (preco <= ? OR ? = 0);";
+
     private static final String CREATE_QUERY =
             "INSERT INTO vinhos.vinhos(nome, ano, descricao, uva, vinicula, regiao, preco," +
                     " quantidade_estoque, img_path, categoria, estilo) " +
@@ -52,10 +53,12 @@ public class PgVinhoDAO implements VinhoDAO{
                     "categoria = CASE WHEN ? IS NOT NULL THEN ? ELSE categoria END, " +
                     "estilo = CASE WHEN ? IS NOT NULL THEN ? ELSE estilo END " +
                     "WHERE numero = ?;";
+
     private static final String DELETE_QUERY =
             "DELETE FROM vinhos.vinhos " +
                     "WHERE numero = ?;";
-    public PgVinhoDAO (Connection connection) {
+
+    public PgVinhoDAO(Connection connection) {
         this.connection = connection;
     }
 
@@ -96,12 +99,15 @@ public class PgVinhoDAO implements VinhoDAO{
                     vinhoResult.setNumero(resultSet.getInt("numero"));
                     vinhoResult.setNome(resultSet.getString("nome"));
                     vinhoResult.setAno(resultSet.getInt("ano"));
+                    vinhoResult.setDescricao(resultSet.getString("descricao"));
                     vinhoResult.setUva(resultSet.getString("uva"));
                     vinhoResult.setVinicula(resultSet.getString("vinicula"));
                     vinhoResult.setRegiao(resultSet.getString("regiao"));
                     vinhoResult.setCategoria(resultSet.getString("categoria"));
                     vinhoResult.setEstilo(resultSet.getString("estilo"));
                     vinhoResult.setPreco(resultSet.getDouble("preco"));
+                    vinhoResult.setQuantidadeEstoque(resultSet.getInt("quantidade_estoque"));
+                    vinhoResult.setImgPath(resultSet.getString("img_path"));
 
                     vinhosList.add(vinhoResult);
                 }
@@ -109,14 +115,14 @@ public class PgVinhoDAO implements VinhoDAO{
 
         } catch (SQLException ex) {
             Logger.getLogger(PgVinhoDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
-            throw new SQLException("Erro ao listar vinhos");
+            throw new SQLException("Erro ao listar vinhos.");
         }
 
         return vinhosList;
     }
 
     @Override
-    public void create (Vinho vinho) throws SQLException{
+    public void create(Vinho vinho) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement(CREATE_QUERY)) {
             statement.setString(1, vinho.getNome());
             statement.setInt(2, vinho.getAno());
@@ -132,32 +138,135 @@ public class PgVinhoDAO implements VinhoDAO{
 
             statement.executeUpdate();
         } catch (SQLException ex) {
-            Logger.getLogger(PgMyAppUserDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
-            throw new SQLException("Hmmm");
+            Logger.getLogger(PgVinhoDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+            throw new SQLException("Erro ao criar vinho.");
         }
     }
 
-    public Vinho read (Integer numero) {
+    @Override
+    public Vinho read(Integer numero) throws SQLException {
+        Vinho vinho = null;
 
-        return null;
+        try (PreparedStatement statement = connection.prepareStatement(READ_QUERY)) {
+            statement.setInt(1, numero);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    vinho = new Vinho();
+                    vinho.setNumero(resultSet.getInt("numero"));
+                    vinho.setNome(resultSet.getString("nome"));
+                    vinho.setAno(resultSet.getInt("ano"));
+                    vinho.setDescricao(resultSet.getString("descricao"));
+                    vinho.setUva(resultSet.getString("uva"));
+                    vinho.setVinicula(resultSet.getString("vinicula"));
+                    vinho.setRegiao(resultSet.getString("regiao"));
+                    vinho.setCategoria(resultSet.getString("categoria"));
+                    vinho.setEstilo(resultSet.getString("estilo"));
+                    vinho.setPreco(resultSet.getDouble("preco"));
+                    vinho.setQuantidadeEstoque(resultSet.getInt("quantidade_estoque"));
+                    vinho.setImgPath(resultSet.getString("img_path"));
+                } else {
+                    throw new SQLException("Erro ao visualizar: vinho não encontrado.");
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PgVinhoDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+            throw new SQLException("Erro ao visualizar vinho.");
+        }
+
+        return vinho;
     }
 
-    public void update (Vinho vinho) {
+    @Override
+    public void update(Vinho vinho) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY)) {
+            statement.setString(1, vinho.getNome());
+            statement.setString(2, vinho.getNome());
 
+            statement.setInt(3, vinho.getAno());
+            statement.setInt(4, vinho.getAno());
+
+            statement.setString(5, vinho.getDescricao());
+            statement.setString(6, vinho.getDescricao());
+
+            statement.setString(7, vinho.getUva());
+            statement.setString(8, vinho.getUva());
+
+            statement.setString(9, vinho.getVinicula());
+            statement.setString(10, vinho.getVinicula());
+
+            statement.setString(11, vinho.getRegiao());
+            statement.setString(12, vinho.getRegiao());
+
+            statement.setDouble(13, vinho.getPreco());
+            statement.setDouble(14, vinho.getPreco());
+
+            statement.setInt(15, vinho.getQuantidadeEstoque());
+            statement.setInt(16, vinho.getQuantidadeEstoque());
+
+            statement.setString(17, vinho.getImgPath());
+            statement.setString(18, vinho.getImgPath());
+
+            statement.setString(19, vinho.getCategoria());
+            statement.setString(20, vinho.getCategoria());
+
+            statement.setString(21, vinho.getEstilo());
+            statement.setString(22, vinho.getEstilo());
+
+            statement.setInt(23, vinho.getNumero());
+
+            if (statement.executeUpdate() < 1) {
+                throw new SQLException("Erro ao editar: vinho não encontrado.");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PgVinhoDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+            throw new SQLException("Erro ao editar vinho.");
+        }
     }
 
-    public void delete (Integer numero) {
+    @Override
+    public void delete(Integer numero) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_QUERY)) {
+            statement.setInt(1, numero);
 
+            if (statement.executeUpdate() < 1) {
+                throw new SQLException("Erro ao excluir: vinho não encontrado.");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PgVinhoDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+            throw new SQLException("Erro ao excluir vinho.");
+        }
     }
 
-    public List<Vinho> all () {
+    @Override
+    public List<Vinho> all() throws SQLException {
+        List<Vinho> vinhosList = new ArrayList<>();
 
-        return null;
+        try (PreparedStatement statement = connection.prepareStatement(ALL_QUERY)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Vinho vinho = new Vinho();
+                    vinho.setNumero(resultSet.getInt("numero"));
+                    vinho.setNome(resultSet.getString("nome"));
+                    vinho.setAno(resultSet.getInt("ano"));
+                    vinho.setDescricao(resultSet.getString("descricao"));
+                    vinho.setUva(resultSet.getString("uva"));
+                    vinho.setVinicula(resultSet.getString("vinicula"));
+                    vinho.setRegiao(resultSet.getString("regiao"));
+                    vinho.setCategoria(resultSet.getString("categoria"));
+                    vinho.setEstilo(resultSet.getString("estilo"));
+                    vinho.setPreco(resultSet.getDouble("preco"));
+                    vinho.setQuantidadeEstoque(resultSet.getInt("quantidade_estoque"));
+                    vinho.setImgPath(resultSet.getString("img_path"));
+
+                    vinhosList.add(vinho);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PgVinhoDAO.class.getName()).log(Level.SEVERE, "DAO", ex);
+            throw new SQLException("Erro ao listar todos os vinhos.");
+        }
+
+        return vinhosList;
     }
-
-
-    /*
-        Implementacao das funcoes, as quais foram declaradas na interfaces:
-        DAO e VinhoDao.
-     */
 }
