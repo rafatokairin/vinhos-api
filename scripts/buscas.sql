a365261205d1fe66c76a87a7f5587d320f1b670b
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2024-10-07 22:44:09 -0300

    Update buscas.sql

--- scripts/buscas.sql
+++ scripts/buscas.sql
@@ -38,27 +38,21 @@ JOIN vinhos.compras cp ON ccv.numero_compra = cp.numero
 WHERE cp.data_registro >= CURRENT_DATE - INTERVAL '3 days'
 
 -- Retorna o nome, quantidade vendida e valor total vendido dos vinhos, num certo período de tempo (dias, semanas, meses, anos)
-SELECT nome, quantidade_vendida, total_vendido
+SELECT v.nome, SUM(ccv.quantidade) AS quantidade_vendida, SUM(ccv.subtotal) AS total_vendido
 FROM vinhos.vinhos v
-JOIN (
-    SELECT numero_vinho AS numero, SUM(quantidade) AS quantidade_vendida, data_registro, SUM(subtotal) AS total_vendido
-    FROM vinhos.compra_carrinho_vinho ccv
-    JOIN vinhos.compras c ON ccv.numero_compra = c.numero
-    GROUP BY numero_vinho, data_registro
-) AS r ON v.numero = r.numero
+JOIN vinhos.compra_carrinho_vinho ccv ON v.numero = ccv.numero_vinho
+JOIN vinhos.compras c ON ccv.numero_compra = c.numero
 WHERE r.data_registro >= CURRENT_DATE - INTERVAL '3 days'
+GROUP BY v.nome
 ORDER BY quantidade_vendida DESC;
 
 -- Retorna o nome, quantidade vendida e valor total vendido dos vinhos, num período entre duas datas 
-SELECT nome, quantidade_vendida, total_vendido
+SELECT v.nome, SUM(ccv.quantidade) AS quantidade_vendida, SUM(ccv.subtotal) AS total_vendido
 FROM vinhos.vinhos v
-JOIN (
-    SELECT numero_vinho AS numero, SUM(quantidade) AS quantidade_vendida, data_registro, SUM(subtotal) AS total_vendido
-    FROM vinhos.compra_carrinho_vinho ccv
-    JOIN vinhos.compras c ON ccv.numero_compra = c.numero
-    GROUP BY numero_vinho, data_registro
-) AS r ON v.numero = r.numero
-WHERE r.data_registro BETWEEN '2024-10-01' AND '2024-10-31'
+JOIN vinhos.compra_carrinho_vinho ccv ON v.numero = ccv.numero_vinho
+JOIN vinhos.compras c ON ccv.numero_compra = c.numero
+WHERE c.data_registro BETWEEN '2024-10-01' AND '2024-10-31'
+GROUP BY v.nome
 ORDER BY quantidade_vendida DESC;
 
 -- Retorna a data e o valor total vendido do dia, para cada dia, num certo período (dias, semanas, meses, anos)
